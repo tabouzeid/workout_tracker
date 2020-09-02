a5d21197104fcc3eec191189214acad372a2a89b
Taher Abouzeid <tabouzeid@gmail.com>
2020-09-02 09:13:27 -0400

    fixing seed Data, fixing schema, add exercise works

---
+++ models/Workout.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  name: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  weight: {
+    type: Number,
+  },
+  sets: {
+    type: Number,
+  },
+  reps: {
+    type: Number,
+  },
+  duration: {
+    type: Number,
+  },
+  duration: {
+    type: Number,
+  },
+});
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date, 
+    default: Date.now,
+  },
+  exercises: [ExerciseSchema],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+const path = require("path");
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
-// const User = require("./userModel.js");
+const db = require("./models");
 const app = express();
 
 app.use(logger("dev"));
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_tracker", 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
 { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -22,8 +22,51 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_tracker
     useFindAndModify: false
 });
 
-app.get("/", () => {
-  
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    .then(workouts => {
+      res.json(workouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.post("/api/workouts", ({body}, res) => {
+    db.Workout.create(body)
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+    console.log(req.body);
+    db.Workout.findOneAndUpdate({_id: req.params.id}, {$push: {exercises: req.body}}, { new: true })
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+    res.end();
 });
 
 app.listen(PORT, () => {
